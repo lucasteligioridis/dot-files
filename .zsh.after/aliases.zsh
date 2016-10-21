90021c1c382813c0bf365106cae9d6cec49418c4
Lucas Teligioridis <lucas.teligioridis@gmail.com>
2016-10-21 19:24:37 +1100

    Add alias file

---
+++ .zsh.after/aliases.zsh
@@ -0,0 +1,5 @@
+alias cppcompile='c++ -std=c++11 -stdlib=libc++'
+alias git=hub
+alias gpa='/Users/lucast/Projects/lucas-workdir/scripts/git/git-pull-all.sh'
+alias zshconfig="subl ~/.zprezto"
+alias envconfig="subl ~/Projects/config/env.sh"
