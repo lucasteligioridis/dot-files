0f308df48e8693d7a218fea2da24e91e1d6a7b03
Lucas Teligioridis <lucas.teligioridis@gmail.com>
2016-10-21 19:24:54 +1100

    Refactor functions into function.zsh

--- .zsh.after/functions.zsh
+++ .zsh.after/functions.zsh
@@ -17,3 +17,10 @@ lexcon () {
 
 # History grep
 function hg() { history 0 | grep -i "$1" }
+
+# FileSearch
+function f() { find . -iname "*$1*" ${@:2} }
+function r() { grep "$1" ${@:2} -R . }
+
+#mkdir and cd
+function mkcd() { mkdir -p "$@" && cd "$_"; }
