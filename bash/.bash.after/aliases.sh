194a26d9139fdc28ca028d092f4c1f3aa5a83318
Lucas Teligioridis <lucas.teligioridis@gmail.com>
2018-08-03 11:10:46 +1000

    Add sudo vim launch to preserve utilities

--- bash/.bash.after/aliases.sh
+++ bash/.bash.after/aliases.sh
@@ -6,3 +6,4 @@ alias ss="pmset displaysleepnow"
 alias grep='grep --exclude="*.pyc" --exclude="*.swp" --exclude="*.tfstate.backup" --color=auto --exclude-dir=.terraform --exclude-dir=.git'
 alias v="f -e vim" # quick opening files with vim
 alias show_apt_installs='( zcat $( ls -tr /var/log/apt/history.log*.gz ) ; cat /var/log/apt/history.log ) | grep -E "^(Start-Date:|Commandline:)" | grep -v aptdaemon | grep -E "^Commandline:"'
+alias rvim='sudo -E vim'
