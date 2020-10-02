e3d984cbc91631b409f855f355fcc387b9d1f1af
Lucas Teligioridis <lucas.teligioridis@gmail.com>
2020-10-02 19:04:44 +1000

    Refactor colors to its own script

---
+++ bin/colors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export NC='\[\033[0m\]'
+export BOLD='\[\033[1m\]'
+export BRIGHT='\[\033[1;37m\]'
+export ORANGE='\[\033[38;5;214m\]'
+export RED='\[\033[31m\]'
+export GREEN='\[\033[1;32m\]'
+export BLUE='\[\033[1;34m\]'
+export TEAL='\[\033[38;5;14m\]'
+export PURPLE='\[\033[1;35m\]'
